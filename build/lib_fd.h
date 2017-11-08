34fac8e71fe1387dc9662771488fb458d476613b
cr7INcs <bmei1314@gmail.com>
2017-11-08 10:09:23 +0800

    add demo

---
+++ build/lib_fd.h
@@ -0,0 +1,18 @@
+#ifndef _TEST_H
+#define _TEST_H
+extern "C" typedef struct Point_Info{
+  int x;
+  int y;
+  int width;
+  int height;
+}Point_Info;
+extern "C" typedef struct Face_Info{
+ int face_num;
+ Point_Info point_Info[10];
+}Face_Info;
+ void lib_dana_fd_init(int minFace,int MaxFace,int Win_step);
+ void lib_dana_fd_proc(int width,int height,unsigned char* img,Face_Info face_info)
+ 
+
+
+#endif
